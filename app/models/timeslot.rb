27d24701ff63d04c287f2f9b3efb35bdd94ffb63
jayzalowitz <jayzalowitz@gmail.com>
2016-02-22 12:46:45 -0800

    Start of booking proc

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -8,10 +8,28 @@ class Timeslot < ActiveRecord::Base
 			id: self.id, 
 			start_time: self.start_time.to_i, 
 			duration: self.duration, 
-			availability: self.boats.map {|boat| boat['capacity']}.reduce(0, :+), 
+			availability: self.max_available_booking, 
 			customer_count: self.bookings.map {|booking| booking['size']}.reduce(0, :+),  
 			boats: self.boats.map{|boat| boat.id} 
 		}
 		return @status
+	end
+
+	def max_available_booking
+		self.match_bookings_to_boats.max
+	end
+
+	def match_bookings_to_boats
+		@boats_to_match = self.boats.map {|boat| {capacity: boat['capacity'], unused_capacity: boat['capacity']} }
+		puts @boats_to_match.inspect
+		self.bookings.each do |booking|
+			@boats_to_match.sort_by { |k| k[:unused_capacity] }
+			puts @boats_to_match.inspect
+			largest_capacity = @boats_to_match.first
+			largest_capacity[:unused_capacity] = largest_capacity[:unused_capacity] - booking.size
+			@boats_to_match[0] = largest_capacity
+		end
+		puts @boats_to_match.inspect
+		return @boats_to_match.map{|boat| boat[:unused_capacity]}
 	end
 end
